bab3f2ac3d74dcc7146fea0a1e1d9a96c5029d9b
Rencelagsil <lagsilrence@gmail.com>
2025-04-01 23:48:23 +0800

    changes

---
+++ ADMIN/admin_login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+  <div class="container1">
+        
+    <!-- Login Form -->
+    <div class="container box" id="signIn">
+      <h1 class="form-title">Welcome Admin!</h1>
+      <form method="post" action="login.php">
+        
+        <div class="input-group">
+          <i class="fas fa-id-card"></i>
+          <input type="text" name="studentID" id="studentIDLogin" placeholder="Username" required>
+          <label for="studentIDLogin">Username</label>
+        </div>
+
+        <div class="input-group">
+          <i class="fas fa-lock"></i>
+          <input type="password" name="password" id="passwordLogin" placeholder="Password" required>
+          <label for="passwordLogin">Password</label>
+        </div>
+
+        <input type="submit" class="btn" value="Log In" name="submit"> 
+
+      </form>
+
+      <div class="Links">
+        <p>Don't have an account?</p>
+        <button id="signUpButton">Sign In</button>  
+      </div>
+    </div>
+
+    <script>
+        // Redirect to enroll.php when "Enroll" button is clicked
+        document.getElementById("signUpButton").addEventListener("click", function() {
+            window.location.href = "enroll.php";
+        });
+    </script>
+
+</body>
+</html>
