5b939476e6ff5f7b52deebaba72643337e4cae6b
RL <142850140+Rencelagsil@users.noreply.github.com>
2025-04-03 22:14:00 +0800

    Update insert_student.php

--- CRUD/insert_student.php
+++ CRUD/insert_student.php
@@ -5,6 +5,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $first_name = $_POST['fName'];
     $middle_name = $_POST['mName'];
     $last_name = $_POST['lName'];
+    $gender = $_POST['gender'];
+    $last_name = $_POST['lName'];
     $birthdate = $_POST['birthdate'];
     $student_id = $_POST['studentID'];
     $street = $_POST['street'];
@@ -14,14 +16,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $zip = $_POST['zip'];
     $email = $_POST['email'];
     $strand = $_POST['strand'];
+    $semester = $_POST['semester'];
     $level = $_POST['level'];
 
     // Insert data into the database
-    $sql = "INSERT INTO students (first_name, middle_name, last_name, birthdate, student_id, street_address, city, state_province, country, zip_code, email, strand, level) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO students (first_name, middle_name, last_name, gender, birthdate, student_id, street_address, city, state_province, country, zip_code, email, strand,semester,level) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?)";
 
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sssssssssssss", $first_name, $middle_name, $last_name, $birthdate, $student_id, $street, $city, $state, $country, $zip, $email, $strand, $level);
+    $stmt->bind_param("sssssssssssssss", $first_name, $middle_name, $last_name, $gender, $birthdate, $student_id, $street, $city, $state, $country, $zip, $email, $strand, $semester, $level);
 
     if ($stmt->execute()) {
         echo "<script>
