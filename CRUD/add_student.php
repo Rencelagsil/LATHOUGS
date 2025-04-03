83b68e1e45e30e24d5e10614286175e75d7b9a7d
RL <142850140+Rencelagsil@users.noreply.github.com>
2025-04-03 22:10:32 +0800

    Update add_student.php

--- CRUD/add_student.php
+++ CRUD/add_student.php
@@ -14,6 +14,11 @@ include 'db.php';
         <input type="text" name="fName" placeholder="First Name" required>
         <input type="text" name="mName" placeholder="Middle Name">
         <input type="text" name="lName" placeholder="Last Name" required>
+        <select name="gender" required>
+        <option value="">Gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+        </select>
         <input type="date" name="birthdate" required>
         <input type="text" name="studentID" placeholder="Student ID" required>
         <input type="text" name="street" placeholder="Street Address" required>
@@ -23,13 +28,20 @@ include 'db.php';
         <input type="text" name="zip" placeholder="ZIP Code" required>
         <input type="email" name="email" placeholder="E-mail" required>
         <select name="strand" required>
+        <option value="">Strand</option>
             <option value="ABM">ABM</option>
             <option value="HUMSS">HUMSS</option>
             <option value="STEM">STEM</option>
             <option value="GAS">GAS</option>
             <option value="TVL">TVL</option>
         </select>
+        <select name="semester" required>
+        <option value="">Semester</option>
+            <option value="1st Semester">1st Semester</option>
+            <option value="2nd Semester">2nd Semester</option>
+        </select>
         <select name="level" required>
+        <option value="">Level</option>
             <option value="Grade 11">Grade 11</option>
             <option value="Grade 12">Grade 12</option>
         </select>
